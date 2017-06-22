3fe90e0205a5e79ca3159446ff7ffb4c2e0190d0
jsteenb2 <jonathansteenbergen@gmail.com>
2017-06-22 18:16:48 -0500

    initial commit, hello quran world
    includes ability to retrieve surah and display it in browser

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"net/http"
+
+	"github.com/boltdb/bolt"
+	m "github.com/jsteenb2/boltDBquran/models"
+	"github.com/labstack/echo"
+)
+
+var (
+	db      *bolt.DB
+	qBucket = []byte("quran")
+)
+
+func main() {
+	e := echo.New()
+	var err error
+	db, err = bolt.Open("/Users/jonathansteenbergen/go/src/quran/quran.db", 0644, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	e.GET("/api", surahFind)
+
+	e.Logger.Fatal(e.Start(":3333"))
+}
+
+func surahFind(c echo.Context) error {
+	suraNum, _ := strconv.Atoi(c.QueryParam("surah"))
+	surah, err := m.GetSurah(qBucket, []byte{byte(suraNum)}, db)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(surah.Name)
+	return c.String(http.StatusOK, surah.String())
+}
